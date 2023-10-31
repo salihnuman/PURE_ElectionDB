144752912521bef8dc50c2e6095f01d7818ed5fc
umursamaz <82212605+umursamaz@users.noreply.github.com>
2023-11-01 00:50:05 +0300

    Update Merge_Excels.py

--- Merge_Excels.py
+++ Merge_Excels.py
@@ -2,15 +2,13 @@ import os
 import pandas as pd
 
 # Set the folder path
-folder_path = 'D:/SU/4 - Senior/Fall/PURE/Seçimler/4 - 31 MART 2019 MAHALLİ İDARELER GENEL SEÇİMİ (31.03.2019) SONUÇLARI/BÜYÜKŞEHİR BELEDİYE BAŞKANLIĞI SEÇİMLERİ'
+folder_path = 'Source_folder_includes_the_xlsx_files'    # Change the path for your files
 
 # Get a list of all Excel files in the folder
 excel_files = [f for f in os.listdir(folder_path) if f.endswith('.xlsx')]
 
 # Create an empty list to store DataFrames
 data_frames = []
-index = 0
-
 
 # Loop through each Excel file in the folder
 for index, file in enumerate(excel_files):
@@ -22,6 +20,5 @@ for index, file in enumerate(excel_files):
 # Concatenate all DataFrames in the list
 merged_data = pd.concat(data_frames, ignore_index=False)
 
-
-# # Write the merged data DataFrame to a new Excel file
-merged_data.to_excel('D:/SU/4 - Senior/Fall/PURE/Seçimler/4 - 31 MART 2019 MAHALLİ İDARELER GENEL SEÇİMİ (31.03.2019) SONUÇLARI Ziplenmiş/BÜYÜKŞEHİR BELEDİYE BAŞKANLIĞI SEÇİMLERİ.xlsx', index=False)
+# Write the merged data DataFrame to a new Excel file
+merged_data.to_excel('The_path_of_created_merged_xlsx_file.xlsx', index=False)    # Change the path for your file
