cd0b26d75d56df0424bf637b7467f8cd911f0b47
salihnuman <115585249+salihnuman@users.noreply.github.com>
2023-11-01 00:29:28 +0300

    Update Merge_Excels.py

--- Merge_Excels.py
+++ Merge_Excels.py
@@ -12,13 +12,6 @@ data_frames = []
 index = 0
 
 
-# data = pd.read_excel(os.path.join(folder_path, excel_files[0]), skiprows=10)
-
-# data2 = pd.read_excel(os.path.join(folder_path, excel_files[1]), skiprows=10)
-# data_frames = pd.concat([data, data2], ignore_index=True)
-# # print(data)
-# print(data_frames)
-
 # Loop through each Excel file in the folder
 for index, file in enumerate(excel_files):
     print(str(index + 1) + " " + file + " is being processed...")
@@ -29,9 +22,6 @@ for index, file in enumerate(excel_files):
 # Concatenate all DataFrames in the list
 merged_data = pd.concat(data_frames, ignore_index=False)
 
-
-# print(merged_data.head())  # Display the first 5 rows
-# print(merged_data.info())  # Display summary information about the DataFrame
 
 # # Write the merged data DataFrame to a new Excel file
 merged_data.to_excel('D:/SU/4 - Senior/Fall/PURE/Seçimler/4 - 31 MART 2019 MAHALLİ İDARELER GENEL SEÇİMİ (31.03.2019) SONUÇLARI Ziplenmiş/BÜYÜKŞEHİR BELEDİYE BAŞKANLIĞI SEÇİMLERİ.xlsx', index=False)
